4c8567c1db5416e15d026a63ef67c0c8c878a999
Shan Lin <40546937+ShanL03@users.noreply.github.com>
2022-03-11 13:51:43 -0800

    Update and rename main.sh to super.sh

---
+++ super.sh
@@ -0,0 +1,17 @@
+# chmod +x main.sh
+
+data_dir="/home/shan/Projects/datasets/3d_data/super_dataset/grasp1_2_psmnet/grasp1_2/exp"
+
+method=super
+phase=test
+
+evaluate_super=1
+data_cost=1
+corr_cost=1
+arap_cost=1
+rot_cost=1
+
+export DISPLAY=:0.0
+python -W ignore main.py --data_dir=${data_dir} --method=${method} --phase=${phase} \
+--evaluate_super=${evaluate_super} --data_cost=${data_cost} --corr_cost=${corr_cost} \
+--arap_cost=${arap_cost} --rot_cost=${rot_cost}
